ed245b19d648e1e51c54c077f42271e53f8b3d23
Maroj Thangthong <celta0992@hotmail.com>
2023-02-05 14:56:22 +0700

    add debounce

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <Bounce2.h>
+#define BUTTON 27
+#define LDR 32
+Bounce debouncer = Bounce();
+int n=0;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("BUTTON");
+  debouncer.attach(BUTTON, INPUT_PULLUP);
+  debouncer.interval(25);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  debouncer.update();
+  if(debouncer.fell()){
+    n++;
+    Serial.println(n);
+  }
+
 }
