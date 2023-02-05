5e59a9c3f9348f7b0ddefd93f12557470c7bbc84
Maroj Thangthong <celta0992@hotmail.com>
2023-02-05 14:01:14 +0700

    update platformio

--- src/main.cpp
+++ src/main.cpp
@@ -6,4 +6,22 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+}#include <Arduino.h>
+#define GREEN 33
+void setup()
+{
+    ledcSetup(0, 5000, 8);
+    ledcAttachPin(GREEN, 0);
+}
+void loop()
+{
+  for (int i = 0; i < 255; i++) {
+    ledcWrite(0, i);
+    delay(5);
+  }
+  for (int i = 255; i > 0; i--) {
+    ledcWrite(0, i);
+    delay(5);
+  }
+  delay(1000);
 }
